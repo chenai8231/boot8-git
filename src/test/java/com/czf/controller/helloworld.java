49c777f3bcebc694cbb6aede6975c6e70439e950
chenai8231 <614880092@qq.com>
2019-12-12 16:09:13 +0800

    首次提交的代码

---
+++ src/test/java/com/czf/controller/helloworld.java
@@ -0,0 +1,13 @@
+package com.czf.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloworld {
+
+    @RequestMapping("hi")
+    public String sayHi(){
+        return "Hello SpringBoot ";
+    }
+}
